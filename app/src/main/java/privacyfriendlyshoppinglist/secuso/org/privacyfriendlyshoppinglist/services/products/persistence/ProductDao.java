e8e9fa43a3332d4116e5886d6fecafbe648c9c9e
Grebiel José Ifill Brito <wh38esoq@rbg.informatik.tu-darmstadt.de>
2016-05-16 15:00:39 +0200

    product Dao and Entity (without implementation)

---
+++ app/src/main/java/privacyfriendlyshoppinglist/secuso/org/privacyfriendlyshoppinglist/services/products/persistence/ProductDao.java
@@ -0,0 +1,10 @@
+package privacyfriendlyshoppinglist.secuso.org.privacyfriendlyshoppinglist.services.products.persistence;
+
+/**
+ * Description:
+ * Author: Grebiel Jose Ifill Brito
+ * Created: 16.05.16 14:59 creation date
+ */
+public interface ProductDao
+{
+}
---
+++ app/src/main/java/privacyfriendlyshoppinglist/secuso/org/privacyfriendlyshoppinglist/services/products/persistence/entity/ProductEntity.java
@@ -0,0 +1,10 @@
+package privacyfriendlyshoppinglist.secuso.org.privacyfriendlyshoppinglist.services.products.persistence.entity;
+
+/**
+ * Description:
+ * Author: Grebiel Jose Ifill Brito
+ * Created: 16.05.16 14:58 creation date
+ */
+public class ProductEntity
+{
+}
