bc05f373877135d28e75c509983b4dbdd4b307d0
Grebiel José Ifill Brito <wh38esoq@rbg.informatik.tu-darmstadt.de>
2016-09-05 23:27:48 +0200

    unnecessary objects removed

--- app/src/main/java/privacyfriendlyshoppinglist/secuso/org/privacyfriendlyshoppinglist/ui/main/listeners/AddOnClickListener.java
+++ app/src/main/java/privacyfriendlyshoppinglist/secuso/org/privacyfriendlyshoppinglist/ui/main/listeners/AddOnClickListener.java
@@ -1,10 +1,6 @@
 package privacyfriendlyshoppinglist.secuso.org.privacyfriendlyshoppinglist.ui.main.listeners;
 
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import privacyfriendlyshoppinglist.secuso.org.privacyfriendlyshoppinglist.framework.context.AbstractInstanceFactory;
-import privacyfriendlyshoppinglist.secuso.org.privacyfriendlyshoppinglist.framework.context.InstanceFactory;
-import privacyfriendlyshoppinglist.secuso.org.privacyfriendlyshoppinglist.logic.shoppingList.business.ShoppingListService;
 import privacyfriendlyshoppinglist.secuso.org.privacyfriendlyshoppinglist.logic.shoppingList.business.domain.ListDto;
 import privacyfriendlyshoppinglist.secuso.org.privacyfriendlyshoppinglist.ui.main.ShoppingListActivityCache;
 import privacyfriendlyshoppinglist.secuso.org.privacyfriendlyshoppinglist.ui.shoppinglist.ListDialogFragment;
@@ -17,13 +13,9 @@ import privacyfriendlyshoppinglist.secuso.org.privacyfriendlyshoppinglist.ui.sho
 public class AddOnClickListener implements View.OnClickListener
 {
     private ShoppingListActivityCache cache;
-    private ShoppingListService shoppingListService;
 
     public AddOnClickListener(ShoppingListActivityCache cache)
     {
-        AppCompatActivity activity = cache.getActivity();
-        AbstractInstanceFactory instanceFactory = new InstanceFactory(activity.getApplicationContext());
-        this.shoppingListService = (ShoppingListService) instanceFactory.createInstance(ShoppingListService.class);
         this.cache = cache;
 
     }
